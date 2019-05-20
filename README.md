cadfc40f330319bdd0cbf838d12a18904bea9868
PraveenkumarBeedanal <st147769@stud.uni-stuttgart.de>
2019-05-20 21:05:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-espvbr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-espvbr)
---
+++ src/app/svg-view/svg-view.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'svg-view',
+  templateUrl: './svg-view.component.html'
+})
+export class SvgComponent  {
+  name = 'Angular';
+}
