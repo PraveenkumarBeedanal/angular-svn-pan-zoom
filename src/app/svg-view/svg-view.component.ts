a068790427a73eac20603b36af3e27b44ac355d6
PraveenkumarBeedanal <st147769@stud.uni-stuttgart.de>
2019-05-20 21:40:55 +0200

    2 files modified, 1 deleted

--- src/app/svg-view/svg-view.component.ts
+++ src/app/svg-view/svg-view.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as svgPanZoom from 'svg-pan-zoom';
 @Component({
   selector: 'svg-view',
   templateUrl: './svg-view.component.html'
 })
-export class SvgComponent  {
-  name = 'Angular';
+export class SvgComponent implements  OnInit {
+
+ngOnInit(){
+
+  const panZoomTiger = svgPanZoom('#svg-id');
+
+}
+
+  
 }
